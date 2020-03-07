c1201a52273a27656f2f111bfa5b1762c644ed35
douglas <douglas06mp@gmail.com>
2020-03-07 15:29:11 +0800

    add media query to PaletteList component

--- src/components/styles/PaletteListStyle.js
+++ src/components/styles/PaletteListStyle.js
@@ -1,3 +1,5 @@
+import breakpoints from '../../helpers/mediaQuery';
+
 export default {
   wrapper: {
     backgroundColor: 'lightblue',
@@ -8,11 +10,21 @@ export default {
   },
   container: {
     width: '50%',
-    marginTop: '50px',
+    marginTop: '30px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+
+    [breakpoints.down('xl')]: {
+      width: '70%'
+    },
+    [breakpoints.down('lg')]: {
+      width: '80%'
+    },
+    [breakpoints.down('xs')]: {
+      width: '75%'
+    }
   },
   nav: {
     width: '100%',
@@ -24,16 +36,34 @@ export default {
 
     '& h1': {
       fontFamily: 'Ubuntu',
-      fontSize: '2rem'
+      fontSize: '2rem',
+      [breakpoints.down('lg')]: {
+        fontSize: '1.8rem'
+      },
+      [breakpoints.down('sm')]: {
+        fontSize: '1.3rem'
+      }
     },
     '& a': {
-      color: '#fff'
+      color: '#fff',
+      [breakpoints.down('md')]: {
+        fontSize: '.8rem'
+      }
     }
   },
   palettesContainer: {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '2.8rem',
+
+    [breakpoints.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 45%)',
+      gridGap: '3.5rem'
+    },
+    [breakpoints.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '2%'
+    }
   }
 };
